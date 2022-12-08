8c99d4a75b592b16159d08b3bbe34e9c6c8e788d
student_1 <student@qwerty.com>
2022-12-08 10:27:36 +0300

    написал программу

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
 ﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равнв 3 символа.
+
+void PrintArray(string[] array) { 
+    for (int i = 0; i < array.Length; i++) {
+        Console.Write($"{array[i]}\t"); 
+    }
+}
+string[] array = {"one","two","three","four","five","six","seven"};
+Console.WriteLine();
+string[] result = new string[array.Length];
+int count = 0;
+for (int i = 0; i < array.Length-1; i++) {
+    if (array[i].Length <= 3) {
+        result[count] = array[i];
+        count++;
+    }
+}
+PrintArray(result);
